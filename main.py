35caa4a37eb0ce2d8743ea745e3915085318d883
Ayka <avesartista@gmail.com>
2025-07-19 17:30:00 +0100

    Created config

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
+from config import MAIN_COLOR, SCREEN_SIZE, CAPTION
 
 pygame.init()
 running = True
-MAIN_COLOR = (255, 255, 255)
 
-screen = pygame.display.set_mode((1400, 1000))
-pygame.display.set_caption("Tora")
+
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption(CAPTION)
 
 while running:
     screen.fill(MAIN_COLOR)
@@ -16,7 +17,6 @@ while running:
     # # Draw the square
     # pygame.draw.rect(screen, game_state['current_color'], (position['x'], position['y'], SQUARE_SIZE, SQUARE_SIZE))
     # pygame.draw.rect(screen, RED, (100, 300, SQUARE_SIZE, SQUARE_SIZE))
-    # pygame.draw.circle(screen, current_color, )
     # Update the display
     pygame.display.flip()
 pygame.quit()
