77cdc94d56edb7567f634f4166b392ac15f5fb48
Aykka <avesartista@gmail.com>
2025-08-05 21:22:59 +0100

    Started level parsing

--- utils/level.py
+++ utils/level.py
@@ -1,4 +1,15 @@
 def getLevel(level_path):
     with open(level_path) as file_object:
-        contents = file_object.read()
-        print(contents)
+        contents = file_object.readlines()
+        for line in contents:
+            print(line.rstrip())
+            for symbol in line:
+                if symbol == '-':
+                    # TODO: CREATE BLOCK HERE
+                    print('This is block')
+                elif symbol == 'o': 
+                    # TODO: CREATE COIN HERE
+                    print('This is coin')
+
+
+getLevel('../levels/1.txt')
