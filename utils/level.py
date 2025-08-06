387c48455ccce566ac8eb14f8492842b9644cdf8
Aykka <avesartista@gmail.com>
2025-08-06 19:59:47 +0100

    added basic coin creation

--- utils/level.py
+++ utils/level.py
@@ -1,3 +1,5 @@
+from sprites.coin import Coin
+
 def getLevel(level_path):
     with open(level_path) as file_object:
         contents = file_object.readlines()
@@ -9,6 +11,7 @@ def getLevel(level_path):
                     print('This is block')
                 elif symbol == 'o': 
                     # TODO: CREATE COIN HERE
+                    coin = Coin(34, 56)
                     print('This is coin')
 
 
